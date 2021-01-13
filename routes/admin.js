ead60724a17db3b792e7bb293c8fabcdc63569cf
rafi-windows <rafi_n_o@github.com>
2021-01-14 05:22:06 +0700

    crud Bank

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
+const { editBank } = require("../controllers/adminController");
 const adminController = require("../controllers/adminController");
+const { upload } = require("../middlewares/multer");
 
 router.get("/dashboard", adminController.viewDashboard);
 // endpoint category
@@ -7,8 +9,12 @@ router.get("/category", adminController.viewCategory);
 router.post("/category", adminController.addCategory);
 router.put("/category", adminController.editCategory);
 router.delete("/category/:id", adminController.deleteCategory);
-
+// endpoint bank
 router.get("/bank", adminController.viewBank);
+router.post("/bank", upload, adminController.addBank);
+router.put("/bank", upload, adminController.editBank);
+router.delete("/bank/:id", adminController.deleteBank);
+
 router.get("/item", adminController.viewItem);
 router.get("/Booking", adminController.viewBooking);
 
